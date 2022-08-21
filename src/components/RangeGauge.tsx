d85e9de5660f79e97fdd0d7e8937d0474abd2140
Andre Miras <andre.miras@gmail.com>
2022-08-21 08:59:56 +0200

    :poop: workaround a gauge resize bug
    Update the gauge height dynamically based on the width, refs:
    https://github.com/Martin36/react-gauge-chart/issues/27

--- src/components/RangeGauge.tsx
+++ src/components/RangeGauge.tsx
@@ -1,12 +1,7 @@
-import { FC } from "react";
+import { FC, useState, useEffect, useRef } from "react";
 import GaugeChart from "react-gauge-chart";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-const chartStyle = {
-  // https://github.com/Martin36/react-gauge-chart/issues/27
-  height: 70,
-};
 
 interface RangeGaugeProps {
   min?: number;
@@ -20,28 +15,48 @@ const RangeGauge: FC<RangeGaugeProps> = ({
   min = 0,
   max = 100,
   label = "",
-}) => (
-  <>
-    <Row>
-      <GaugeChart
-        percent={(value - min) / max}
-        animate={false}
-        formatTextValue={() => value.toFixed(1)}
-        style={chartStyle}
-      />
-    </Row>
-    <Row className="justify-content-end">
-      <Col sm={3} className="fs-6">
-        {min}
-      </Col>
-      <Col sm={5} className="fs-6 fw-bold">
-        {label}
-      </Col>
-      <Col sm={3} className="fs-6">
-        {max}
-      </Col>
-    </Row>
-  </>
-);
+}) => {
+  /*
+   * We have to dynamically reset the height on width change, refs:
+   * https://github.com/Martin36/react-gauge-chart/issues/27
+   */
+  const [height, setHeight] = useState(0);
+  const chartStyle = {
+    height,
+  };
+  const ref = useRef<HTMLDivElement>(null);
+  // listen to window resize events to update the chart height, refs gauge bug
+  useEffect(() => {
+    const onResize = () =>
+      ref.current !== null && setHeight(ref.current.clientWidth / 2.2);
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
+  return (
+    <>
+      <Row ref={ref}>
+        <GaugeChart
+          percent={(value - min) / max}
+          animate={false}
+          formatTextValue={() => value.toFixed(1)}
+          style={chartStyle}
+        />
+      </Row>
+      <Row className="justify-content-end">
+        <Col sm={3} className="fs-6">
+          {min}
+        </Col>
+        <Col sm={5} className="fs-6 fw-bold">
+          {label}
+        </Col>
+        <Col sm={3} className="fs-6">
+          {max}
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default RangeGauge;
