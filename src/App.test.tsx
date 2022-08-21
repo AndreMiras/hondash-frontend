23298b565958b2330224567806c37487b860bfe3
Andre Miras <andre.miras@gmail.com>
2022-08-21 16:45:55 +0200

    :white_check_mark: Fix App tests enable tests in CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders a gauge", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const gaugeElement = screen.getByText("Speed");
+  expect(gaugeElement).toBeInTheDocument();
 });
